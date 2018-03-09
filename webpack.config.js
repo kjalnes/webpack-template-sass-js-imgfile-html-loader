2c3f4c19b4bf95eede5d247a4d08d4b81ca1e3e2
Kristine Alnes <kris.alnes@gmail.com>
2018-03-09 13:32:22 -0500

    Webpack config template with sass, es6, html, img file loader and clean-webpack-plugin that removes dist folder before each build

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebPackPlugin = require('clean-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({
+   filename: 'main.css'
+});
+
+
+module.exports = {
+    entry: './client/src/App.js',  // can be array, can be object with aliases or string
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+        // publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
+            }, {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[text]',
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        extractPlugin,
+        new HtmlWebpackPlugin({
+            template: 'client/src/index.html'
+        }),
+        new CleanWebPackPlugin(['dist'])
+    ]
+};
